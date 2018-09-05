8039aab02d95fe3288292e937d5f10204aeec2f4
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-09-05 13:26:36 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+        int b = (179 + 21 + 10) / 7 + 181;
+        int c = 10389 * 56 * 11 + 2246;
+
+        String sA = "79 + 3 * (4 + 82 - 68) - 7 + 19 = " + a;
+        String sB = "(179 + 21 + 10) / 7 + 181 = " + b;
+        String sC = "10389 * 56 * 11 + 2246 = " + c;
+
+        System.out.println(sA);
+        System.out.println(sB);
+        System.out.println(sC);
+
+    }
+}
